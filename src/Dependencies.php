eeb0c9e52f8bda0c5dcf5b1c0a9be64cef379e5a
idzhalalov <idzhalalov@gmail.com>
2019-05-20 18:26:27 +0500

    A simple dependencies container was involved

---
+++ src/Dependencies.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Language;
+
+
+class Dependencies
+{
+    protected static $instances = [];
+    protected static $dependencies = [
+        'DATA_SERVICE_PROVIDER' => 'Language\SystemApiStrategy',
+        'OUTPUT_PROVIDER' => 'Language\StdOutStrategy'
+    ];
+
+    public static function getInstance($key) {
+        if (!isset(self::$dependencies[$key])) {
+            throw new \Exception('Unknown dependency ' . $key);
+        }
+
+        if (!isset(self::$instances[$key])) {
+            self::$instances[$key] = new self::$dependencies[$key]();
+        }
+
+        return self::$instances[$key];
+    }
+}
